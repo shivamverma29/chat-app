11b02253ac5c28230661e6b90d0e5eb8edb5578a
Shivam Verma <91418627+shivamverma29@users.noreply.github.com>
2024-06-09 13:05:06 +0530

    server side for logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -74,4 +74,12 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = (req, res) => {};
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
